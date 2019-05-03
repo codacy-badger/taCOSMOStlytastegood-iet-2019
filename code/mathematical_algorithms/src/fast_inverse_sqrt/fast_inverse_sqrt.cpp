fea8b5562da39905a3845205b32a297f0f059cfb
Gergo <tomcsanyigergely@gmail.com>
2019-05-03 23:51:37 +0200

    Integer replaced with fixed-with integer int32_t and C-style cast changed to reinterpret_cast.

--- code/mathematical_algorithms/src/fast_inverse_sqrt/fast_inverse_sqrt.cpp
+++ code/mathematical_algorithms/src/fast_inverse_sqrt/fast_inverse_sqrt.cpp
@@ -7,11 +7,12 @@
 // Part of Cosmos by OpenGenus Foundation
 
 #include "fast_inverse_sqrt.hpp"
+#include <cstdint>
 
 double fastInverseSqrt(double x)
 {
     float xhalf = 0.5f * x;
-    int i = *(int*)&x;              // get bits for floating value
+    int32_t i = reinterpret_cast<int32_t>(x);              // get bits for floating value
     i = 0x5f375a86 - (i >> 1);      // gives initial guess y0
     x = *(float*)&i;                // convert bits back to float
     x = x * (1.5f - xhalf * x * x); // Newton step, repeating increases accuracy
