48a1e25cc67e206a2afcedd57ba7056ecb27d89a
Gergo <tomcsanyigergely@gmail.com>
2019-05-04 15:03:21 +0200

    Magic number fixed.

--- code/mathematical_algorithms/src/fast_inverse_sqrt/fast_inverse_sqrt.cpp
+++ code/mathematical_algorithms/src/fast_inverse_sqrt/fast_inverse_sqrt.cpp
@@ -13,7 +13,7 @@ double fastInverseSqrt(double x)
 {
     float xhalf = 0.5f * x;
     int64_t i = reinterpret_cast<int64_t>(x);              // get bits for floating value
-    i = 0x5f375a86 - (i >> 1);      // gives initial guess y0
+    i = 0x5fe6eb50c7b537a9 - (i >> 1);      // gives initial guess y0
     x = reinterpret_cast<double>(i);                // convert bits back to float
     x = x * (1.5f - xhalf * x * x); // Newton step, repeating increases accuracy
     return x;
