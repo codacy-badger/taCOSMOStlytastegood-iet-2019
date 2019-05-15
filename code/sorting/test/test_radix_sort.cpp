9f262f0dd2d51ed844ca5c88fd5f180316241080
Gergo <tomcsanyigergely@gmail.com>
2019-05-15 16:32:06 +0200

    Test changed in radix_sort

--- code/sorting/test/test_radix_sort.cpp
+++ code/sorting/test/test_radix_sort.cpp
@@ -4,6 +4,9 @@
 
 TEST_CASE("test one", "[sample]") {
 	std::vector<int> test_data{};
+	test_data.push_back(2);
+	test_data.push_back(3);
+	test_data.push_back(1);
 	radix_sort(test_data);
-    REQUIRE(test_data.size() == 0);
+    REQUIRE(test_data[0] == 1);
 }
